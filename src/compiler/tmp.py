16c7349c7889cb601f1c539aab9a590097222e8e
aestriplex <aestriplex@post.com>
2020-04-04 22:16:16 +0200

    simple functions done

--- src/compiler/tmp.py
+++ src/compiler/tmp.py
@@ -15,6 +15,14 @@ class DepthTypeException(Exception) :
     def __str__(self) :
         return self.msg
 
+class BodyTypeException(Exception) :
+    
+    def __init__(self,_t) :
+        self.msg = f"Depth must be a list, not {_t.__name__}"
+    
+    def __str__(self) :
+        return self.msg
+
 class ScriptTypeException(Exception) :
     
     def __init__(self,_t) :
@@ -23,13 +31,13 @@ class ScriptTypeException(Exception) :
     def __str__(self) :
         return self.msg
 
-class Body :
+class VarTypeException(Exception) :
     
-    def __init__(self,_type) :
-        self._type = _type
+    def __init__(self,_t) :
+        self.msg = f"You must pass a string for parameter {_t.__name__} insted of {type(_t).__name__}"
     
     def __str__(self) :
-        return f"{type(self._type).__name__}"
+        return self.msg
     
 class VarKind(Enum) :
     var = 0
@@ -41,54 +49,79 @@ class VarType :
 
 class Function :
 
-    def __init__(self) :
+    _name = None
+    _block = None
+    _params = []
+
+    def __init__(self,src) :
+        self._parse_fun(src)
+
+    def __str__(self) :
+        return f"<FUN: {self._name}>"
+
+    def _parse_fun(self,src) :
+        self._name = src.id.name
+        self._params = [Variable(p.name,"var") for p in src.params]
+        self._block = parse(src.body.body)
+    
+class Block :
+
+    depth = None
+    body = []
+
+    def __init__(self,src,depth) :
+        if type(src) != list :
+            raise BodyTypeException(type(src))
         pass
 
     def __str__(self) :
-        pass
+        return f"<Block depth: {self.depth}>"
 
 class Variable :
 
-    _name = None
-    _kind = None
-    _value = None
+    def __init__(self,name,kind,value = None) :
+        if type(name) != str :
+            raise VarTypeException(name)
+
+        self._name = name
+        self._kind = kind
+        self._value = value
     
-    def __init__(self, src, kind) :
-        
-        self._kind = self._get_kind(kind)
-        self._parse_var(src)
-        
     def __str__(self) :
-        return f"<{self._name}, {self._value}, {self._kind}>"
-    
-    def _parse_var(self,src) :
-        self._name = src.id.name
-        self._value = self._get_value(src)
-    
-    def _get_value(self,src) :
-        if src.init.type == VarType.literal :
-            return src.init.value
-        if src.init.type == VarType.obj :
-            return self._parse_object(src.init.properties)
-    
-    def _parse_object(self,prop) :
-        obj = {}
-        for p in prop :
-            obj.update({p.key.value : p.value.value})
-        return obj
-    
-    def _get_kind(self,k) :
+        return f"<VAR: {self._name}>"
+
+def _parse_variable(src, kind) :   
+    kind = _get_kind(kind)
+    name = src.id.name
+    value = _get_var_value(src)
+    return Variable(name,kind,value)    
+
+def _get_var_value(src) :
+    if src.init.type == VarType.literal :
+        return src.init.value
+    if src.init.type == VarType.obj :
+        return _parse_object(src.init.properties)
+
+def _parse_object(prop) :
+    obj = {}
+    for p in prop :
+        obj.update({p.key.value : p.value.value})
+    return obj
+
+def _get_kind(k) :
 
         if k == "var" :
             return VarKind.var
         if k == "const" :
             return VarKind.const
+        else :
+            return None
 
-def _parse_variable(declarations, kind) :
+def _get_variables(declarations, kind) :
     
     v = []
     for d in declarations :
-        v.append(Variable(d,kind))
+        v.append(_parse_variable(d,kind))
     if v != [] :
         return v
     
@@ -102,9 +135,10 @@ def parse(src) :
 
     body = []
     for e in src :
+        if is_function(e.type) :
+            body.append(Function(e))
         if is_variable(e.type) :
-            body += _parse_variable(e.declarations,e.kind)
-
+            body += _get_variables(e.declarations,e.kind)
     return body
 
 with open("/Users/teo/Desktop/notebooks/a.js","r") as f :
@@ -116,4 +150,4 @@ except Exception as ex:
     exit()
 print(source.body)
 l = parse(source.body)
-#print(*l)
+print(*l)
