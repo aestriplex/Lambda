7fa6cee7f40392f98574f54efe234864eda85828
aestriplex <aestriplex@post.com>
2020-04-05 22:53:28 +0200

    support to call functions

--- src/compiler/tmp.py
+++ src/compiler/tmp.py
@@ -1,8 +1,12 @@
 import esprima
 from enum import Enum
+from abc import ABC, abstractmethod
+from z3 import *
+import time
 
 var = "VariableDeclaration"
 expr = "ExpressionStatement"
+call = "CallExpression"
 up_expr = "UpdateExpression"
 bin_expr = "BinaryExpression"
 ass_expr = "AssignmentExpression"
@@ -13,6 +17,9 @@ do_while_statement = "DoWhileStatement"
 fun_declaration = "FunctionDeclaration"
 fun_expr = ["FunctionExpression","ArrowFunctionExpression"]
 declarator = ["VariableDeclarator","ObjectDeclarator"]
+
+occ = {}
+assertions = []
 
 class DepthTypeException(Exception) :
     
@@ -45,6 +52,22 @@ class VarTypeException(Exception) :
     
     def __str__(self) :
         return self.msg
+
+class KindTypeException(Exception) :
+    
+    def __init__(self) :
+        self.msg = f"Kind unknown"
+    
+    def __str__(self) :
+        return self.msg
+
+class TypeException(Exception) :
+
+    def __init__(self,_t) :
+        self.msg = f"{_t.__name__} must inherits from `Instruction`"
+    
+    def __str__(self) :
+        return self.msg
     
 class VarKind(Enum) :
     var = 0
@@ -64,7 +87,13 @@ class VarType :
     literal = "Literal"
     obj = "ObjectExpression"
 
-class Expression :
+class Exe(ABC) :
+
+    @abstractmethod
+    def to_ssa(self) :
+        pass
+
+class Expression(Exe) :
 
     def __init__(self,kind,operator,first,second = None) :
         self.kind = kind
@@ -74,8 +103,23 @@ class Expression :
 
     def __str__(self) :
         return f"<Expr: {self.operator}>"
+    
+    def to_ssa(self) :
+        pass
 
-class Conditional :
+class Call(Exe) :
+
+    def __init__(self,callee,params) :
+        self._callee = callee
+        self._params = params
+
+    def __str__(self) :
+        return f"<CALL {self._callee}>"
+
+    def to_ssa(self) :
+        pass
+
+class Conditional(Exe) :
 
     def __init__(self,test,if_block,else_block) :
         self.test = test
@@ -84,8 +128,11 @@ class Conditional :
 
     def __str__(self) :
         return f"<Conditional>"
+    
+    def to_ssa(self) :
+        pass
 
-class Loop :
+class Iteration(Exe) :
 
     def __init__(self,kind,test,body) :
         self.kind = kind
@@ -94,18 +141,25 @@ class Loop :
     
     def __str__(self) :
         return f"<Loop: {self.kind}>"
+    
+    def to_ssa(self) :
+        pass
 
-class Function :
+class Fun(Exe) :
 
-    def __init__(self,src) :
-        self._name = src.id.name
-        self._params = [Variable(p.name,"var") for p in src.params]
-        self._block = parse(src.body.body)
+    def __init__(self,name,params,body,isasync) :
+        self._name = name
+        self._params = params
+        self._body = body
+        self._async = isasync
 
     def __str__(self) :
         return f"<FUN: {self._name}>"
+    
+    def to_ssa(self) :
+        pass
 
-class Variable :
+class Variable(Exe) :
 
     def __init__(self,name,kind,value = None) :
         if type(name) != str :
@@ -117,6 +171,19 @@ class Variable :
     
     def __str__(self) :
         return f"<VAR: {self._name}>"
+    
+    def to_ssa(self) :
+        if type(self._value) == int :
+            val = Int(self._name)
+        elif type(self._value) == float :
+            val = Real(self._name)
+        if self._name not in occ :
+            occ.update({self._name : [val]})
+        else :
+            pass
+        i = len(occ[self._name])
+        if self._value is not None :
+            assertions.append(occ[self._name][i-1] == self._value)
 
 def _parse_variable(src, kind) :   
     kind = _get_kind(kind)
@@ -136,37 +203,43 @@ def _parse_object(prop) :
         obj.update({p.key.value : p.value.value})
     return obj
 
+def _parse_call(src) :
+    callee = src.callee.name
+    params = [a.value for a in src.arguments]
+    return Call(callee,params)
+
 def _get_kind(k) :
 
-        if k == "var" :
-            return VarKind.var
-        if k == "const" :
-            return VarKind.const
-        if k == bin_expr :
-            return ExprKind.binary
-        if k == up_expr :
-            return ExprKind.update
-        if k == ass_expr :
-            return ExprKind.assignment
-        if k == while_statement :
-            return LoopKind.while_loop
-        if k == do_while_statement :
-            return LoopKind.do_while_loop
-        if k == for_statement :
-            return LoopKind.for_loop
-        else :
-            return None
+    if k == "var" :
+        return VarKind.var
+    if k == "const" :
+        return VarKind.const
+    if k == bin_expr :
+        return ExprKind.binary
+    if k == up_expr :
+        return ExprKind.update
+    if k == ass_expr :
+        return ExprKind.assignment
+    if k == while_statement :
+        return LoopKind.while_loop
+    if k == do_while_statement :
+        return LoopKind.do_while_loop
+    if k == for_statement :
+        return LoopKind.for_loop
+    else :
+        raise KindTypeException()
 
 def _get_variables(declarations, kind) :
     
     v = []
     for d in declarations :
-        if d.type in fun_expr :
-            v.append(Function(d))
+        if d.init.type in fun_expr :
+            d.init.id = d.id
+            v.append(_parse_fun(d.init))
         else :
             v.append(_parse_variable(d,kind))
-    if v != [] :
-        return v
+
+    return v
 
 def _parse_expr(src) :
     kind = _get_kind(src.type)
@@ -183,6 +256,12 @@ def _parse_expr(src) :
     
     return Expression(kind,src.operator,first,second)
 
+def _parse_fun(src) :
+    name = src.id.name
+    params = [Variable(p.name,"var") for p in src.params]
+    body = parse(src.body.body)
+    return Fun(name,params,body,src.isAsync)
+
 def _parse_conditional(src) :
     condition = _parse_expr(src.test)
     if_block = parse(src.consequent.body)
@@ -195,7 +274,7 @@ def for_to_while(src) :
     body = parse(src.body.body)
     test = _parse_expr(src.test)
     body.append(increment)
-    loop = Loop(LoopKind.while_loop,test,body)
+    loop = Iteration(LoopKind.while_loop,test,body)
     init =  _get_variables(src.init.declarations,src.kind)
     init.append(loop)
     return init
@@ -207,7 +286,7 @@ def _parse_loop(src) :
     else :
         test = _parse_expr(src.test)
         body = parse(src.body.body)
-        return [Loop(kind,test,body)]
+        return [Iteration(kind,test,body)]
     
 def is_variable(element) :
     return element.type == var
@@ -225,23 +304,30 @@ def is_loop(element) :
     return element.type == for_statement or \
             element.type == while_statement or \
             element.type == do_while_statement
+        
+def is_call(element) :
+    return element.type == expr and \
+            element.expression.type == call
 
 def parse(src) :
 
     body = []
     for e in src :
         if is_function(e) :
-            body.append(Function(e))
-        if is_variable(e) :
+            body.append(_parse_fun(e))
+        elif is_variable(e) :
             body += _get_variables(e.declarations,e.kind)
-        if is_conditional(e) :
+        elif is_conditional(e) :
             body.append(_parse_conditional(e))
-        if is_expression(e) :
+        elif is_call(e) :
+            body.append(_parse_call(e.expression))
+        elif is_expression(e) :
             body.append(_parse_expr(e.expression))
-        if is_loop(e) :
+        elif is_loop(e) :
             body += _parse_loop(e)
     return body
 
+start = time.time()
 with open("/Users/teo/Desktop/notebooks/a.js","r") as f :
     aaa = f.read()
 try :
@@ -252,3 +338,8 @@ except Exception as ex:
 print(source.body)
 l = parse(source.body)
 print(*l)
+for e in l :
+    e.to_ssa()
+stop = time.time()
+print(f"Time: {stop-start}")
+print("*** END ***")
