08fcc9f13a64e1c49365a27f8199351a4c23d7f9
aestriplex <aestriplex@post.com>
2020-08-30 18:44:56 +0200

    fixed bug on context; Condition done

--- src/lmb/context.py
+++ src/lmb/context.py
@@ -15,8 +15,11 @@ class Context :
     """
     """
     def __init__(self, parent: Context = None) -> None :
-        self._occurrencies = {}
-        self._types = {}
+        if parent is None :
+            self._occurrencies = {}
+            self._types = {}
+        else :
+            self._occurrencies, self._types = self._get_from_parent(parent)
         self._parent = parent
 
     def __src__(self) -> str :
@@ -26,6 +29,13 @@ class Context :
     def __repr__(self) -> str :
         var = "var" if len(self._occurrencies) == 1 else "vars"
         return f"<Context ({len(self._occurrencies)} {var}) at {hex(id(self))}>"
+
+    def _get_from_parent(self, parent_ctx: Context) -> tuple :
+        parent_occ, parent_types = parent_ctx.get_content()
+        return parent_occ.copy(), parent_types.copy()
+    
+    def get_content(self) -> tuple :
+        return self._occurrencies, self._types
 
     def add(self, occurrence: str, _type: Any = None) -> None :
         """
@@ -45,17 +55,21 @@ class Context :
         label = re.sub(remove_ctx_index,"",occurrence)
         
         if label not in self._types :
+            if self._parent is not None :
+                return self._parent.get_type(occurrence)
             raise VariableMissingException(label)
 
         return self._types[label]
 
-    def set__parent(self, parent: Context) -> None :
+    def set_parent(self, parent: Context) -> None :
         self._parent = parent
 
     def get_label(self, var_name: str, var_type: Label) -> str :
         """
         """
         if var_name not in self._occurrencies :
+            if self._parent is not None :
+                return self._parent.get_label(var_name, var_type)
             raise VariableMissingException(var_name)
 
         n_occurrence = self._occurrencies[var_name]
